192cc29ddaeecf35493b93b9650abefd2c65acdf
Sergei Yarushin <work45-ru@mail.ru>
2023-04-18 00:43:19 +0500

    pre_final

--- registry/admin.py
+++ registry/admin.py
@@ -17,7 +17,29 @@ from.models import Purchase
 
 
 class PurchaseAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    ...
+    list_display = ('hospital_name',
+                    'name',
+                    'cost',
+                    'arrive_data',
+                    'sent_fin_data',
+                    'sent_hospital_data',
+                    'sent_correct_data',
+                    'comment_text',
+                    'p9',
+                    'repair',
+                    'develop_DED',
+                    'PDF_scan',
+                    )
+
+    search_fields = ('hospital_name',
+                     'name',
+                     'cost',
+                     'arrive_data',
+                     'sent_fin_data',
+                     'sent_hospital_data',
+                     'sent_correct_data',
+                     )
 
 
 admin.site.register(Purchase, PurchaseAdmin)
+
